b0bf1fdabac335f80991540af6fe56284b3fc15a
jtr-code <jtrtricks@gmail.com>
2025-07-25 23:06:27 +0530

    add: integrate compression middleware and update package dependencies

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
+import compression from "compression";
 import { apiLimiter } from "./utils/rateLimiter";
 import { errorHandler } from "./middlewares/errorHandler";
 
@@ -13,6 +14,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
+app.use(compression());
 app.use(apiLimiter);
 
 // routes import
